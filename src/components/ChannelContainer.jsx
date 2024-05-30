75b17f5def93a7c0a35ed6e1d793903924128a7d
Prerna Dabi <prernadabi24@gmail.com>
2024-05-30 15:49:43 +0530

    Update ChannelContainer.jsx

--- src/components/ChannelContainer.jsx
+++ src/components/ChannelContainer.jsx
@@ -33,11 +33,11 @@ const ChannelContainer = ({
     <div className="channel-empty__container">
       <p className="channel-empty__first">
         {" "}
-        This is the beginning of your chat history.
+        Have fun chatting.. !! This is the beginning of your chat history.!
       </p>
       <p className="channel-empty__second">
         {" "}
-        Send messages, attachments, links, emojis, and more !{" "}
+        Send messages, attachments, links, emojis, and more! {" "}
       </p>
     </div>
   );
